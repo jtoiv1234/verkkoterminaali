46e0ede042513221ce923a01e035bb42c5fcc46b
jtoiv1234 <34055758+jtoiv1234@users.noreply.github.com>
2017-12-05 13:38:51 +0200

    Create userController.js

---
+++ api/controllers/userController.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+User = mongoose.model('User');
+
+exports.getUsers = function(req, res) {
+  Tuote.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.getUserById = function(req, res) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
+
+exports.createUser = function(req, res) {
+  var newUser = new User(req.body);
+  newUser.save(function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
+
+exports.deleteUser = function(req, res) {
+  User.remove({
+    _id: req.params.userId
+  }, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'User successfully deleted' });
+  });
+};
+
+exports.updateUser = function(req, res) {
+  User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
